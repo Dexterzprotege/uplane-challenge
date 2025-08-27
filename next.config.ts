9ca1e17a1fdc60832f579999245cd073479944fc
Dexterzprotege <sharath.kamlekar@outlook.com>
2025-08-27 22:17:48 +0200

    fix: build error fix babel compiler

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export', // Outputs a Single-Page Application (SPA).
-  // distDir: './dist', // Changes the build output directory to `./dist/`.
-  experimental: {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+    experimental: {
     reactCompiler: true,
   },
-}
- 
-export default nextConfig
+};
+
+export default nextConfig;
